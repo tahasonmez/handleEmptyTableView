aa6af886ef42038e75dfa5fa09cda4b5eb6ea2e1
Taha Sönmez <info@tahasonmez.com>
2018-06-27 01:51:51 +0300

    Initial Commit

---
+++ handleEmptyTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  handleEmptyTableView
+//
+//  Created by Taha Sönmez on 27.06.2018.
+//  Copyright © 2018 Taha Sönmez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
